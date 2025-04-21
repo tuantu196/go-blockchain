f1ac27f33569d776b24fbe038222a7e6719c3330
tube <tuantu196@gmail.com>
2025-04-21 11:35:12 +0700

    example blockchain

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type Block struct {
+	TimeStamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+type Person struct {
+	Name string
+}
+
+func main() {
+	bc := NewBlockChain()
+	fmt.Println("tube", bc)
+
+	bc.AddBlock("The second")
+	bc.AddBlock("The 3rd")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+
+	person := NewPerson("test")
+	person1 := NewPerson1("test1")
+	fmt.Println("tube", person)
+	fmt.Println("tube1", *person1)
+}
+
+func NewPerson(name string) Person {
+	person := Person{name}
+	return person
+}
+func NewPerson1(name string) *Person {
+	person := &Person{name}
+	return person
+}
+
+func (b *Block) SetHash() {
+	timeStamp := []byte(strconv.FormatInt(b.TimeStamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timeStamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
+
+func (bc *BlockChain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{NewGenesisBlock()}}
+}
